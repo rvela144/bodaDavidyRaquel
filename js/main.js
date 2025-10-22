9e69ec474a481c535290dcb7e3151d24730c88e8
rvela144 <serverprueb1@gmail.com>
2025-10-22 10:12:40 +0200

    nueva versión portada y icono de carga

--- js/main.js
+++ js/main.js
@@ -2,6 +2,47 @@ const overlay = document.getElementById("portadaBienvenida");
 const musica= document.getElementById("musicaFondo");
 const btnConMusica = document.getElementById("btnConMusica");
 const btnSinMusica = document.getElementById("btnSinMusica");
+const modal = new bootstrap.Modal(document.getElementById('portadaBienvenida'));
+/*MÉTODO MOSTRAR ICONO DE CARGA */
+
+window.addEventListener('load', function () {
+  var preloader = document.querySelector('.loader');
+  var preloaderArea = document.querySelector('.cargarIcono');
+  //oculto todo el contenido menos el icono de carga
+ // Mostrar el loader
+
+  // Fade out loader
+if (preloader) {
+    preloader.style.transition = 'opacity 0.5s ease';
+    preloader.style.opacity = 0;
+    setTimeout(() => {
+      preloader.style.display = 'none';
+    }, 500);
+  }
+
+  // Fade out preloader area con delay de 100ms
+  if (preloaderArea) {
+    setTimeout(() => {
+      preloaderArea.style.transition = 'opacity 0.5s ease';
+      preloaderArea.style.opacity = 0;
+      setTimeout(() => {
+        preloaderArea.style.display = 'none';
+      }, 500);
+    }, 100);
+  }
+
+  // Detener animación de carga
+  animLoader.destroy();
+
+
+    modal.show();
+  // Iniciar animaciones de portada
+ // animFlores1.setSpeed(0.6);
+ // animFlores1.play();
+  //animFlores2.setSpeed(0.6);
+  //animFlores2.play();*/
+});
+
 
 
 let player;
@@ -24,18 +65,16 @@ function onYouTubeIframeAPIReady() {
 /* funcion que se ejecuta cuando se ha terminado de cargar el reproductor */
   function onPlayerReady(event) {
           event.target.setVolume(80);
-    console.log("Player listo");
+
     /** evento para cuando entras con música */
       document.getElementById('btnConMusica').addEventListener('click',(e)=>
         {
           e.preventDefault();
           document.getElementById('play-pause-music').setAttribute('data-estado-music', 'play');
          try{
-           // player.unMute();
-          //  player.setVolume(80);
             animMusicAnimIcon.play();
             player.playVideo();
-            cerrarPortadaBienvenida();
+           cerrarPortadaBienvenida();
           }catch (err){
             console.error(err);
           }
@@ -72,14 +111,15 @@ function pauseVideo() {
      
 
 function cerrarPortadaBienvenida(){
-
-  overlay.classList.add('hidden');
-  setTimeout(()=>(overlay.style.display='none'),700);
+//ahora como es modal se hace asi 
+modal.hide();
+ // overlay.classList.add('hidden');
+  //setTimeout(()=>(overlay.style.display='none'),700);
 
 
 }
 
-
+//ANIMACIÓN NOTA MUSCIAL
      let svgContainerMusicAnimIcon = document.querySelector('.music-anim-icon');
 
     let animMusicAnimIcon = bodymovin.loadAnimation({
@@ -90,4 +130,15 @@ function cerrarPortadaBienvenida(){
     path: "img/iconos/notaMusical.json"
 
     });     
+
+  //ANIMACION ICONO RECARGAR
+      let svgContainerLoader = document.querySelector('.loader');
+
+  let animLoader = bodymovin.loadAnimation({
+    container: svgContainerLoader,
+    renderer: 'svg',
+    loop: true,
+    path: "img/iconos/corazon.json"
+  });
+
 
