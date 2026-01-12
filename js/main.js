81cb81a3f075b20f19585998d6680153b933e56d
rvela144 <serverprueb1@gmail.com>
2026-01-12 20:05:31 +0100

    prueba añadir sugerir canciones

--- js/main.js
+++ js/main.js
@@ -739,6 +739,17 @@ document.body.addEventListener("click", async function (e) {
   }
 });
 
+/* Funciones y varialbes globales */
+
+  // Funcion para validar formato url
+  function is_url(str) {
+    regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(str)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 /*fetch("/api/sugerir_cancion", {
   method: "POST",
