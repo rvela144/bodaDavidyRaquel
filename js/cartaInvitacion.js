f90ab0e4c0890245689cd146b588970a07a3e997
rvela144 <serverprueb1@gmail.com>
2026-01-16 15:34:30 +0100

    carta añadida

---
+++ js/cartaInvitacion.js
@@ -0,0 +1,48 @@
+(() => {
+  let animando = false;
+
+  const env = document.getElementById("env");
+  const sealBtn = document.getElementById("sealBtn");
+  const btnContinuar = document.getElementById("btnContinuar");
+
+  if (!env || !sealBtn) return;
+
+  function abrir() {
+    if (animando) return;
+    animando = true;
+
+    env.classList.add("is-seal-broken");
+
+    setTimeout(() => env.classList.add("is-opening"), 250);
+   
+    setTimeout(() => env.classList.add("is-paper-out"), 1150);
+      setTimeout(() => env.classList.add("is-paper-unfold"), 2150);
+        setTimeout(() => env.classList.add("is-paper-drop"), 2200);
+    // setTimeout(() => env.classList.add("is-paper-peek"), 2950);
+ 
+    // setTimeout(() => env.classList.add("is-paper-out"), 2150);
+     //setTimeout(() => env.classList.add("is-paper-drop"), 2300);
+  
+
+    setTimeout(() => {
+      env.classList.add("is-message");
+      animando = false;
+    }, 2150);
+  }
+
+  sealBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    abrir();
+  });
+
+  if (btnContinuar) {
+    btnContinuar.addEventListener("click", (e) => {
+       e.preventDefault();
+
+    // marca que ya vio la carta
+    sessionStorage.setItem("mostrarCarta", "false");
+    // vuelve al index principal
+    window.location.href = "../index.html";
+    });
+  }
+})();
