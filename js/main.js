4c9fba768290a4d6d674da9bde6be6999983d4eb
rvela144 <serverprueb1@gmail.com>
2026-03-01 15:04:06 +0100

    Añadido , modal asistencia

--- js/main.js
+++ js/main.js
@@ -906,6 +906,19 @@ document.body.addEventListener("click", async function (e) {
     if (form) form.insertAdjacentHTML("afterend", `<span id="error-form">Error de conexión.</span>`);
   }
 });
+//Cerrar modal asistencia
+//funcion para cerrar sugerir cancion 
+document.body.addEventListener("click", function (e) {
+  const btn = e.target.closest("#cerrarModalAsistencia");
+  if (!btn) return;
+
+  e.preventDefault();
+
+  const modalEl = document.getElementById("modalSugerirCancion");
+  const modal = bootstrap.Modal.getOrCreateInstance(modalEl);
+
+  modal.hide();
+});
 // enlace para abrir modal asistencia
 document.body.addEventListener("click", function (e) {
   const link = e.target.closest("a.confirmar-asistencia");
